5a7bdd75850cfedef928408ae0eda5e56ba19ff0
Slizz19 <salisa.na@gmail.com>
2023-02-21 13:09:54 +0700

    0

--- src/views/save.vue
+++ src/views/save.vue
@@ -32,27 +32,27 @@
           <td>1</td>
           <td>
         <tr v-for="(item, index) in getdata[0]" :key="index">
-          <td v-if="getdata[0][index].RP_property"><br>{{ getdata[0][index].RP_property }} </td>
+          <td ><br>{{ getdata[0][index].RP_property }} </td>
         </tr>
         </td>
         <td>
           <tr v-for="(item, index) in getdata[0]" :key="index">
-            <td v-if="getdata[0][index].RP_property"><br>{{ getdata[0][index].RP_property_number }}</td>
+            <td ><br>{{ getdata[0][index].RP_property_number }}</td>
           </tr>
         </td>
         <td>
           <tr v-for="(item, index) in getdata[0]" :key="index">
-            <td v-if="getdata[0][index].RP_property"><br>{{ getdata[0][index].RP_disrepair }}</td>
+            <td ><br>{{ getdata[0][index].RP_disrepair }}</td>
           </tr>
         </td>
         <td >
           <tr v-for="(item, index) in getdata[0]" :key="index" >
-            <td v-if="getdata[0][index].RP_property" ><br>{{ getdata[0][index].RC_limit }}</td>
+            <td><br>{{ getdata[0][index].RC_limit }}</td>
           </tr>
         </td>
         <td>
           <tr v-for="(item, index) in getdata[0]" :key="index">
-            <td v-if="getdata[0][index].RP_property"><br>{{ getdata[0][index].RC_latest_limit }}</td>
+            <td><br>{{ getdata[0][index].RC_latest_limit }}</td>
           </tr>
         </td>
         </tr>
